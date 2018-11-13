c5d7f2b084b821dfb23565bd3a6ca56e53064ba4
Rich Gerdes <richard.gerdes@rutgers.edu>
2018-11-13 10:09:06 -0500

    Fixes
    > PHP Fatal error:  Class RoyGoldman\DrupalEnvSettings\Plugin contains 1 abstract method and must therefore be declared abstract or implement the remaining methods (Composer\Plugin\PluginInterface::activate)

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,6 +2,8 @@
 
 namespace RoyGoldman\DrupalEnvSettings;
 
+use Composer\Composer;
+use Composer\IO\IOInterface;
 use Composer\Plugin\Capable;
 use Composer\Plugin\PluginInterface;
 
@@ -17,6 +19,13 @@ class Plugin implements PluginInterface, Capable {
     return [
       'Composer\Plugin\Capability\CommandProvider' => 'RoyGoldman\DrupalEnvSettings\CommandProvider',
     ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function activate(Composer $composer, IOInterface $io) {
+    // Do nothing.
   }
 
 }
